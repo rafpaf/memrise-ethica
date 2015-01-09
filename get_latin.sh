f525e4c90c8f6ead5e00e0140599d52697892954
Raphael Krut-Landau <raphael.kl@gmail.com>
2015-01-09 13:49:31 -0500

    hasty commit without summary

--- get_latin.sh
+++ get_latin.sh
@@ -2,4 +2,4 @@
 cd ~/bin/words-1.97Ed
 latin=`./words $@ | grep "\[" -m 1 | cut -d " " -f 1 | sed 's/,//g'`
 english=`./words $@ | grep ";" -m 1 | cut -d ";" -f 1`
-echo "$latin, \"$english\""
+echo "$latin    $english"
